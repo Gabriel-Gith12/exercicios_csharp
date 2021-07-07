73ce74bb10315959c37c56a1cb5adc68aeb8f7bf
Gabriel-Gith12 <86987936+Gabriel-Gith12@users.noreply.github.com>
2021-07-07 17:25:19 -0300

    Add files via upload

---
+++ ExercicioResolvido1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExercicioResolvido1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double x =3;
+           double y =4.0;
+           double z = -5;
+           double A, B, C;
+           
+           A = Math.Sqrt(x);
+           B = Math.Sqrt(y);
+           C = Math.Sqrt(25.0);
+
+           Console.WriteLine("Raiz quadrada de " + x +" = " + A);
+           Console.WriteLine("Raiz quadrada de " + y +" = " + B);
+           Console.WriteLine("Raiz quadrada de 25 = " + C);
+
+           A = Math.Pow(x, y);
+           A = Math.Pow(x, 2.0);
+           A = Math.Pow(5.0, 2.0);
+
+           Console.WriteLine(" elevado a " + y +" = " + A);
+           Console.WriteLine(" elevado ao quadrado = " + B);
+           Console.WriteLine("5 elevado ao quadrado = " + C);
+
+           A = Math.Abs(y);
+           B = Math.Abs(y);
+           
+           Console.WriteLine("Valor absoluto de" + x +" = " + A);
+           Console.WriteLine(" Valor absoluto de " + z +" = " + B);
+
+           Console.ReadLine();
+
+           
+        }
+    }
+}
---
+++ ExercicioResolvido3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ExercicioResolvido3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fazer um programa para ler o nome (apenas uma palavra) e 
+            // idade de duas pessoas. Ao final mostrar uma mensagem com os nomes e a 
+            // idade média entre essas pessoas, com uma casa decimal, conforme exemplo.
+            // Exemplo: Entrada: Nairo 53
+            //                   Rafael 32
+            // Saída
+            // A idade média de Nairo e Rafael é de x  anos
+            
+            string frase, nome1, nome2;
+            string[] vetor;
+            int idade1, idade2;
+            double media;
+
+            frase = Console.ReadLine();
+            vetor = frase.Split(' ');
+            nome1 = vetor[0];
+            idade1 = int.Parse(vetor[1]);
+
+            frase = Console.ReadLine();
+            vetor = frase.Split(' ');
+            nome2 = vetor[0];
+            idade2 = int.Parse(vetor[1]);
+
+            media = (double) (idade1 + idade2) / 2.0;
+            Console.WriteLine("A idade média de " + nome1 + " e " + nome2 + " é de " + media.ToString("F1")+" anos.");
+
+        }
+    }
+}
