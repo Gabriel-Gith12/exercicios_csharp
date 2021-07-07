2a95e006da4dbf8a0e5c8d94ecaf770dbed36d86
Gabriel-Gith12 <86987936+Gabriel-Gith12@users.noreply.github.com>
2021-07-07 17:29:02 -0300

    Add files via upload

---
+++ AreaTrapezio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AreaTrapezio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaracao das variaveis
+            double b, B, h, area;
+
+            // inicializacao
+            b = 5.0;
+            B = 6.0;
+            h = 12.0;
+
+            // processamento
+            area = (b + B) / 2.0 * h;
+            Console.WriteLine(area);
+        }
+    }
+}
---
+++ Checklist/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Checklist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+
+            Console.WriteLine("digite uma string");
+            string s = Console.ReadLine();
+
+            string a,b,c;
+
+            Console.WriteLine("digite a primeira palavra");
+            string s = Console.ReadLine();
+
+            Console.WriteLine("digite a segunda palavra");
+            string s = Console.ReadLine();
+
+            Console.WriteLine("digite a terceira palavra");
+            string s = Console.ReadLine();
+
+            string[] frase = Console.ReadLine().Split(" ");
+            string x,y,z;
+            x = frase[0];
+            y = frase[1];
+            z = frase[2];
+
+            Console.WriteLine("voce digitou");
+            Console.WriteLine(s);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+        }
+    }
+}
