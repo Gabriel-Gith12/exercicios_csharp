1209226443ae3b1f80fab94908d3eb199a5092f9
Gabriel-Gith12 <86987936+Gabriel-Gith12@users.noreply.github.com>
2021-07-07 17:23:12 -0300

    Add files via upload

---
+++ PrimeiroApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrimeiroApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade, sexo, altura;
+
+            int idade = 22;
+            char sexo = 'M';
+            double altura = 1.75;
+
+            Console.WriteLine(idade: {0},Idade);
+            Console.WriteLine(Sexo: {0},Sexo);
+            Console.WriteLine(Altura: {0},Altura);
+        }
+    }
+}
