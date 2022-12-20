08c3efef38eae46b6e001764534f4b3522230326
Gabriel Camargo <gabriel.camargo421@gmail.com>
2022-12-19 21:31:43 -0300

    Buscador de CEP

---
+++ WF_CepMaterialSkin/WF_CepMaterialSkin/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace WF_CepMaterialSkin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTheme = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtCEP = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtEndereco = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtComplemento = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtComplemento2 = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtBairro = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtCidade = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtEstado = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.btnBuscar = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnTheme
+            // 
+            this.btnTheme.Depth = 0;
+            this.btnTheme.Location = new System.Drawing.Point(12, 381);
+            this.btnTheme.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnTheme.Name = "btnTheme";
+            this.btnTheme.Primary = true;
+            this.btnTheme.Size = new System.Drawing.Size(75, 23);
+            this.btnTheme.TabIndex = 35;
+            this.btnTheme.Text = "Dark";
+            this.btnTheme.UseVisualStyleBackColor = true;
+            this.btnTheme.Click += new System.EventHandler(this.btnTheme_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtCEP);
+            this.groupBox1.Controls.Add(this.txtEndereco);
+            this.groupBox1.Controls.Add(this.txtComplemento);
+            this.groupBox1.Controls.Add(this.txtComplemento2);
+            this.groupBox1.Controls.Add(this.txtBairro);
+            this.groupBox1.Controls.Add(this.txtCidade);
+            this.groupBox1.Controls.Add(this.txtEstado);
+            this.groupBox1.Controls.Add(this.btnBuscar);
+            this.groupBox1.Location = new System.Drawing.Point(12, 78);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(408, 297);
+            this.groupBox1.TabIndex = 34;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CEP";
+            // 
+            // txtCEP
+            // 
+            this.txtCEP.Depth = 0;
+            this.txtCEP.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCEP.Hint = "____-___";
+            this.txtCEP.Location = new System.Drawing.Point(6, 19);
+            this.txtCEP.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtCEP.Name = "txtCEP";
+            this.txtCEP.PasswordChar = '\0';
+            this.txtCEP.SelectedText = "";
+            this.txtCEP.SelectionLength = 0;
+            this.txtCEP.SelectionStart = 0;
+            this.txtCEP.Size = new System.Drawing.Size(224, 23);
+            this.txtCEP.TabIndex = 39;
+            this.txtCEP.UseSystemPasswordChar = false;
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Depth = 0;
+            this.txtEndereco.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEndereco.Hint = "Endereço";
+            this.txtEndereco.Location = new System.Drawing.Point(6, 62);
+            this.txtEndereco.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.PasswordChar = '\0';
+            this.txtEndereco.SelectedText = "";
+            this.txtEndereco.SelectionLength = 0;
+            this.txtEndereco.SelectionStart = 0;
+            this.txtEndereco.Size = new System.Drawing.Size(388, 23);
+            this.txtEndereco.TabIndex = 38;
+            this.txtEndereco.UseSystemPasswordChar = false;
+            // 
+            // txtComplemento
+            // 
+            this.txtComplemento.Depth = 0;
+            this.txtComplemento.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtComplemento.Hint = "Complemento";
+            this.txtComplemento.Location = new System.Drawing.Point(6, 105);
+            this.txtComplemento.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtComplemento.Name = "txtComplemento";
+            this.txtComplemento.PasswordChar = '\0';
+            this.txtComplemento.SelectedText = "";
+            this.txtComplemento.SelectionLength = 0;
+            this.txtComplemento.SelectionStart = 0;
+            this.txtComplemento.Size = new System.Drawing.Size(388, 23);
+            this.txtComplemento.TabIndex = 37;
+            this.txtComplemento.UseSystemPasswordChar = false;
+            // 
+            // txtComplemento2
+            // 
+            this.txtComplemento2.Depth = 0;
+            this.txtComplemento2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtComplemento2.Hint = "Complemento 2";
+            this.txtComplemento2.Location = new System.Drawing.Point(6, 143);
+            this.txtComplemento2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtComplemento2.Name = "txtComplemento2";
+            this.txtComplemento2.PasswordChar = '\0';
+            this.txtComplemento2.SelectedText = "";
+            this.txtComplemento2.SelectionLength = 0;
+            this.txtComplemento2.SelectionStart = 0;
+            this.txtComplemento2.Size = new System.Drawing.Size(388, 23);
+            this.txtComplemento2.TabIndex = 36;
+            this.txtComplemento2.UseSystemPasswordChar = false;
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Depth = 0;
+            this.txtBairro.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBairro.Hint = "Bairro";
+            this.txtBairro.Location = new System.Drawing.Point(6, 182);
+            this.txtBairro.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.PasswordChar = '\0';
+            this.txtBairro.SelectedText = "";
+            this.txtBairro.SelectionLength = 0;
+            this.txtBairro.SelectionStart = 0;
+            this.txtBairro.Size = new System.Drawing.Size(388, 23);
+            this.txtBairro.TabIndex = 35;
+            this.txtBairro.UseSystemPasswordChar = false;
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Depth = 0;
+            this.txtCidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCidade.Hint = "Cidade";
+            this.txtCidade.Location = new System.Drawing.Point(6, 221);
+            this.txtCidade.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.PasswordChar = '\0';
+            this.txtCidade.SelectedText = "";
+            this.txtCidade.SelectionLength = 0;
+            this.txtCidade.SelectionStart = 0;
+            this.txtCidade.Size = new System.Drawing.Size(388, 23);
+            this.txtCidade.TabIndex = 34;
+            this.txtCidade.UseSystemPasswordChar = false;
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Depth = 0;
+            this.txtEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEstado.Hint = "Estado";
+            this.txtEstado.Location = new System.Drawing.Point(6, 260);
+            this.txtEstado.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.PasswordChar = '\0';
+            this.txtEstado.SelectedText = "";
+            this.txtEstado.SelectionLength = 0;
+            this.txtEstado.SelectionStart = 0;
+            this.txtEstado.Size = new System.Drawing.Size(388, 23);
+            this.txtEstado.TabIndex = 33;
+            this.txtEstado.UseSystemPasswordChar = false;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Depth = 0;
+            this.btnBuscar.Location = new System.Drawing.Point(253, 19);
+            this.btnBuscar.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Primary = true;
+            this.btnBuscar.Size = new System.Drawing.Size(101, 23);
+            this.btnBuscar.TabIndex = 31;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 433);
+            this.Controls.Add(this.btnTheme);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "CEP";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialRaisedButton btnTheme;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtCEP;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtEndereco;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtComplemento;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtComplemento2;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtBairro;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtCidade;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtEstado;
+        private MaterialSkin.Controls.MaterialRaisedButton btnBuscar;
+    }
+}
+
---
+++ WF_CepMaterialSkin/WF_CepMaterialSkin/Form1.cs
@@ -0,0 +1,69 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_CepMaterialSkin
+{
+    public partial class Form1 : MaterialForm
+    {
+        MaterialSkinManager materialSkinManager;
+        public Form1()
+        {
+            InitializeComponent();
+            materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            // Configure color schema
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Green900, Primary.BlueGrey900,
+                Primary.BlueGrey500, Accent.Orange700,
+                TextShade.WHITE
+            );
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            using (var ws = new WSCorreios.AtendeClienteClient())
+            {
+                try
+                {
+                    var resultado = ws.consultaCEP(txtCEP.Text);
+                    txtEndereco.Text = resultado.end;
+                    txtComplemento.Text = resultado.complemento2;
+                    txtCidade.Text = resultado.cidade;
+                    txtBairro.Text = resultado.bairro;
+                    txtEstado.Text = resultado.uf;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnTheme_Click(object sender, EventArgs e)
+        {
+            if (materialSkinManager.Theme == MaterialSkinManager.Themes.DARK)
+            {
+                btnTheme.Text = "DARK";
+                materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+
+            }
+            else
+            {
+                btnTheme.Text = "LIGHT";
+                materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+
+            }
+        }
+    }
+}
