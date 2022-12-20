f9288a773f3f0f6af82e2de1456220a692feaafb
Gabriel Camargo <gabriel.camargo421@gmail.com>
2022-12-19 22:11:24 -0300

    Calculadora de IMC

---
+++ WF_ImcMaterialSkin/WF_ImcMaterialSkin/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace WF_ImcMaterialSkin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            "MENOR QUE 18,5",
+            "MAGREZA",
+            "0",
+            ""}, -1);
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
+            "ENTRE 18,5 E 24,9",
+            "NORMAL",
+            "0"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem(new string[] {
+            "ENTRE 25,0 E 29,9",
+            "SOBREPESO",
+            "|"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem(new string[] {
+            "ENTRE 30,0 E 39,9",
+            "OBESIDADE",
+            "||"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem(new string[] {
+            "MAIOR QUE 40,0",
+            "OBESIDADE GRAVE",
+            "|||"}, -1);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.txtAltura = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtPeso = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.btnCalcular = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.btnLimpar = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lvIMC = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnTheme = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Depth = 0;
+            this.txtAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAltura.Hint = "Altura (ex: 1,70)";
+            this.txtAltura.Location = new System.Drawing.Point(6, 19);
+            this.txtAltura.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.PasswordChar = '\0';
+            this.txtAltura.SelectedText = "";
+            this.txtAltura.SelectionLength = 0;
+            this.txtAltura.SelectionStart = 0;
+            this.txtAltura.Size = new System.Drawing.Size(172, 23);
+            this.txtAltura.TabIndex = 5;
+            this.txtAltura.UseSystemPasswordChar = false;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Depth = 0;
+            this.txtPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPeso.Hint = "Peso(ex: 69,2)";
+            this.txtPeso.Location = new System.Drawing.Point(201, 19);
+            this.txtPeso.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.PasswordChar = '\0';
+            this.txtPeso.SelectedText = "";
+            this.txtPeso.SelectionLength = 0;
+            this.txtPeso.SelectionStart = 0;
+            this.txtPeso.Size = new System.Drawing.Size(142, 23);
+            this.txtPeso.TabIndex = 6;
+            this.txtPeso.UseSystemPasswordChar = false;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Depth = 0;
+            this.btnCalcular.Location = new System.Drawing.Point(6, 68);
+            this.btnCalcular.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Primary = true;
+            this.btnCalcular.Size = new System.Drawing.Size(172, 23);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Depth = 0;
+            this.btnLimpar.Location = new System.Drawing.Point(201, 68);
+            this.btnLimpar.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Primary = true;
+            this.btnLimpar.Size = new System.Drawing.Size(142, 23);
+            this.btnLimpar.TabIndex = 8;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtAltura);
+            this.groupBox1.Controls.Add(this.btnLimpar);
+            this.groupBox1.Controls.Add(this.txtPeso);
+            this.groupBox1.Controls.Add(this.btnCalcular);
+            this.groupBox1.Location = new System.Drawing.Point(12, 75);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(391, 129);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cálculo";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lvIMC);
+            this.groupBox2.Location = new System.Drawing.Point(12, 210);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(465, 201);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Interpretação do IMC";
+            // 
+            // lvIMC
+            // 
+            this.lvIMC.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvIMC.FullRowSelect = true;
+            this.lvIMC.HideSelection = false;
+            this.lvIMC.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3,
+            listViewItem4,
+            listViewItem5});
+            this.lvIMC.Location = new System.Drawing.Point(9, 19);
+            this.lvIMC.MultiSelect = false;
+            this.lvIMC.Name = "lvIMC";
+            this.lvIMC.Size = new System.Drawing.Size(450, 176);
+            this.lvIMC.TabIndex = 0;
+            this.lvIMC.UseCompatibleStateImageBehavior = false;
+            this.lvIMC.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "IMC";
+            this.columnHeader1.Width = 113;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "CLASSIFICAÇÃO";
+            this.columnHeader2.Width = 127;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "OBESSIDADE(GRAU)";
+            this.columnHeader3.Width = 125;
+            // 
+            // btnTheme
+            // 
+            this.btnTheme.Depth = 0;
+            this.btnTheme.Location = new System.Drawing.Point(12, 422);
+            this.btnTheme.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnTheme.Name = "btnTheme";
+            this.btnTheme.Primary = true;
+            this.btnTheme.Size = new System.Drawing.Size(75, 23);
+            this.btnTheme.TabIndex = 33;
+            this.btnTheme.Text = "Dark";
+            this.btnTheme.UseVisualStyleBackColor = true;
+            this.btnTheme.Click += new System.EventHandler(this.btnTheme_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 457);
+            this.Controls.Add(this.btnTheme);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "IMC";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtAltura;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtPeso;
+        private MaterialSkin.Controls.MaterialRaisedButton btnCalcular;
+        private MaterialSkin.Controls.MaterialRaisedButton btnLimpar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView lvIMC;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private MaterialSkin.Controls.MaterialRaisedButton btnTheme;
+    }
+}
+
---
+++ WF_ImcMaterialSkin/WF_ImcMaterialSkin/Form1.cs
@@ -0,0 +1,70 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_ImcMaterialSkin
+{
+    public partial class Form1 : MaterialForm
+    {
+        MaterialSkinManager materialSkinManager;
+        public Form1()
+        {
+            InitializeComponent();
+            // Criando um material theme manager e adicionando o formulário
+            materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            // Definindo um esquema de Cor para formulário com tom Azul
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Blue400, Primary.Blue500,
+                Primary.Blue500, Accent.LightBlue200,
+                TextShade.WHITE
+            );
+        }
+        private void imc()
+        {
+            double peso, altura, imc;
+
+            peso = double.Parse(txtPeso.Text);
+            altura = double.Parse(txtAltura.Text);
+
+            imc = peso / (altura * altura);
+
+            MessageBox.Show("Seu IMC é: " + imc.ToString("F2"), "Resultado IMC", MessageBoxButtons.OK);
+        }
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtAltura.Text = string.Empty;
+              txtPeso.Text = string.Empty;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            imc();
+        }
+
+        private void btnTheme_Click(object sender, EventArgs e)
+        {
+            if (materialSkinManager.Theme == MaterialSkinManager.Themes.DARK)
+            {
+                btnTheme.Text = "DARK";
+                materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+
+            }
+            else
+            {
+                btnTheme.Text = "LIGHT";
+                materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+
+            }
+        }
+    }
+}
